f6baeb3bd7f8dfadf324f70762eb5e9ed28824a7
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-05-13 20:20:10 +0500

    ..

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-// 2.Evaluating a number game:
-// • Prompt the user to enter a number.
-// • Compare the entered number with a dynamic number value.
-// • Output the result indicating whether the ente5red number is greater than, equal to, or less 
-// than the dynamic number value.
+// 2.Evaluating a number game: // • Prompt the user to enter a number.
+// • Compare the entered number with a dynamic number value. // • Output the result indicating 
+// whether the ente5red number is greater than, equal to, or less than the dynamic number value.
 import inquirer from "inquirer";
 let dynamicNumber = Math.round(Math.random() * 100);
 console.log(dynamicNumber);
 async function dynamic() {
     let userInput = await inquirer.prompt([{
-            name: "Number",
-            type: "input",
-            message: "Enter the Number:"
+            name: "Number", type: "input", message: "Enter the Number:"
         }]);
-    // Compare the entered number with the dynamic number value
     let userNumber = (userInput.Number);
-    // Compare the entered number with the dynamic number value
-    if (userNumber < dynamicNumber) {
+    if (userNumber < dynamicNumber) { // Compare the entered number with the dynamic number value
         console.log("Your number is less than the dynamic number.");
     }
     else if (userNumber > dynamicNumber) {
